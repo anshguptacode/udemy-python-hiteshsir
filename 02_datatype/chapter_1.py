0a04933eb1af11cd121dc2b0e072f42ab67ce787
Ansh <anshguptacode@gmail.com>
2025-07-23 01:29:03 +0530

    Mutable and immutable

--- 02_datatype/chapter_1.py
+++ 02_datatype/chapter_1.py
@@ -5,6 +5,28 @@ print(f"Second Amount :{sugar_ammount}")
 print(f"ID of 2: {id(2)}")
 print(f"ID of 12: {id(12)}")
 """
-HERE WHAT WE LEARN IS THAT THE MUTABLILTY OF ANY OBJECT CAN BE 
-CHECKED BY IDENTITY DON'T CHECK IT BY VALUE
+sugar_ammount = 2:
+An integer object with the value 2 is created in memory. Let's assume its unique memory address (identity) is ID_A.
+The variable sugar_ammount is then assigned to reference this integer object at ID_A.
+print(id(sugar_ammount)) will output ID_A.
+sugar_ammount = 12:
+This is an assignment operation. Since integers are immutable data types in Python, 
+the existing integer object 2 at ID_A cannot be modified in-place to become 12.
+Instead, a new integer object with the value 12 is created in a different memory location. Let's assume its unique memory address is ID_B.
+The variable sugar_ammount is then reassigned (rebound) to now reference this new integer object at ID_B.
+print(id(sugar_ammount)) will now output ID_B.
+print(f"ID of integer object 2: {id(2)}"):
+This explicitly requests the identity of the integer object 2. Python might have a cached instance of 2 (for small integers,
+Python often pre-allocates them for efficiency), and it will return ID_A.
+print(f"ID of integer object 12: {id(12)}"):
+This explicitly requests the identity of the integer object 12. It will return ID_B.
+
+#The crucial point:
+The id of sugar_ammount changed between the two assignment statements because sugar_ammount stopped pointing to the object with value 2 and 
+started pointing to a different object with value 12. This behavior is characteristic of operations involving immutable types: any "modification" 
+effectively creates a new object.
+
+## In This what i am changing is the reference not the values (means i cannot change the value of 2 or 12)
+what the python in the backend is doing is changeing the reference of sugar_ammount from 2 then to 12.
+
 """
--- 02_datatype/chapter_2.py
+++ 02_datatype/chapter_2.py
@@ -1,5 +1,18 @@
 spice_mix =set()
 print(f"Inital Spice Mix ID : {id(spice_mix)}")
+print(f"Inital Spice Mix : {spice_mix}")
 spice_mix.add("ginger")
 spice_mix.add("Dalchini")
 print(f"Afer spice Mix ID:{id(spice_mix)}")
+print(f"Afer spice Mix :{spice_mix}")
+'''
+Inital Spice Mix ID : 4371042752
+Inital Spice Mix : set()
+Afer spice Mix ID:4371042752
+Afer spice Mix :{'Dalchini', 'ginger'}
+
+    # HERE AS WE CAN SEE THE ID REMAINS THE SAME FOR THE SPICE MIX
+    BUT WE CAN STILL ADD THE NEW VALUES INTO THE SET WHICH MEANS SET IS OF MUTABLE TYPE
+    THERE ARE MORE SUCH AS LIST DICTIONARIES ETC.
+    
+'''
