73ee082ec1bbef1c117f75f40bc6b5867683e19a
Ansh <anshguptacode@gmail.com>
2025-07-23 20:40:03 +0530

    dara type

---
+++ 02_datatype/chapter_3.py
@@ -0,0 +1,20 @@
+#interger
+black_tea_grams=3
+gigner_tea_gram=2
+total_grams= black_tea_grams+gigner_tea_gram
+print(f"Total Grams: {total_grams}")
+
+milk_in_liters=5
+serving=4
+milk_per_serving=milk_in_liters/serving
+print(f"Milk per serving:{milk_per_serving}")
+
+total_tea_bags=4
+total_pots=3
+bags_per_pot=total_tea_bags//total_pots
+print(f"bags per pot:{bags_per_pot}")
+
+total_cadamon_pods=10
+pods_per_cups=3
+remaining_pods=total_cadamon_pods%pods_per_cups
+print(f"Total Remaing pods:{remaining_pods}")
