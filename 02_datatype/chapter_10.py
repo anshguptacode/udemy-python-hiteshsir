a88df02340c47610b8d77b754c0b29737df82991
Ansh <anshguptacode@gmail.com>
2025-07-24 23:11:48 +0530

    updated

--- 02_datatype/chapter_10.py
+++ 02_datatype/chapter_10.py
@@ -11,3 +11,19 @@ black tea and milk as the vales for the keys
 '''
 print(chai_recipe["base"])
 del chai_recipe["liquid"]#to delete the element
+print(chai_recipe)
+
+#membership
+print(f"is sugar avaliable in chai recipe? {"sugar" in chai_recipe}")
+
+print(f"Keys of chai_order: {chai_order.keys()}")
+print(f"Values of chai_order: {chai_order.values()}")
+print(f"Items of chai_order: {chai_order.items()}")
+last_item=chai_order.popitem()
+print(last_item)
+print(chai_order)
+extra_spices=dict( cardmom= "crushed",ginger= "sliced")
+chai_recipe.update(extra_spices)
+print(f"updated chai recipe: {chai_recipe}")
+chai_size=chai_order.get("customer note","not found")
+print(chai_size)
